e5effbaf4fcea7a2be1bb8be5b91615b376719b0
akhmetov <alexander.akhmetov@locotech-signal.ru>
2020-02-09 20:51:16 +0300

    add connectors

--- TreeDrawer.cpp
+++ TreeDrawer.cpp
@@ -25,16 +25,24 @@ void TreeDrawer::drawTree(TreeItem *item)
 	_drawer->addRectangle(item->x(), item->y(), item->width(), item->height());
 	_drawer->addText(item->x(), item->y(), item->text());
 
+	int parentCenterX = item->x() + item->width() / 2;
+	int parentBottomY = item->y() + item->height();
+
 	for (auto child : item->children())
 	{
 		drawTree(child);
 
 		// Draw connector
 		{
-			// int
-			// int verticalDistance =
-			// _drawer->addLine(item->x() + item->width() / 2, item->y() + item->height(), );
+			int	childCenterX = child->x() + child->width() / 2;
+			int childTopY = child->y();
+			
+			int verticalDistance = childTopY - parentBottomY;
+			
+			_drawer->addLine(parentCenterX, parentBottomY, parentCenterX, parentBottomY + verticalDistance / 2);
+			_drawer->addLine(childCenterX, childTopY, childCenterX, childTopY - verticalDistance / 2);
 
+			_drawer->addLine(parentCenterX, parentBottomY + verticalDistance / 2, childCenterX, childTopY - verticalDistance / 2);
 		}
 	}
 }
